e76882411dd83082f8a9ab4ad8275ff9b2bfdca6
Lai Ze Min <zeminlai426@gmail.com>
2023-06-21 20:27:07 +0800

    add calendar to events page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Sidepanel from './components/Sidepanel';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Events  from './pages/Events';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Dashboard/>} />
             <Route path='/projects' element={<Projects/>} />
+            <Route path='/events' element={<Events/>} />
           </Routes>
       </Router>
       </>
