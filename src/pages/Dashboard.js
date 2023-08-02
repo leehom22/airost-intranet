d414c81c13de6bf1d3b2646447ed7c8e26bdf38e
Lai Ze Min <zeminlai426@gmail.com>
2023-08-03 00:19:55 +0800

    change sidepanel width to vw

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,10 +4,17 @@ import './Dashboard.css';
 function Dashboard() {
   return (
     <div className='dashboard'>
-      <div className="container">
-        <img src="profile_pic.jpeg" alt="Profile" className='profile-pic'/>
+      <div className="header-container">
         <div className="background"><img src="profile_bg.png" alt="Profile background" /></div>
-        <div className="aboutSection"></div>
+  
+        <div className="profile-section">
+          <img src="profile_pic.jpeg" alt="Profile" className='profile-pic'/>
+          <div className="about-section">
+            <div className="about-header">About
+              <div className="profile-description">Passionate about Machine Learning and Data Science! 3rd year Software Engineering student</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
