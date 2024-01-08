a64ab78579379a22db2fd396b2b6dfae4eeeeb05
Lai Ze Min <106502102+zeminlai@users.noreply.github.com>
2024-01-08 14:35:37 +0800

    Created show all users feature in Admin page

--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -1,8 +1,12 @@
 import './Events.css'
 
 function Events() {
-    return ( <div className="eventsPage">
-        <center><h1>Events page</h1></center>
+    return ( 
+    <div className="eventsPage col">
+        <h1 className='row justify-content-center'>Events page</h1>
+        <div className='row justtify-content-center ps-5 pe-5'>
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%233F51B5&ctz=Asia%2FKuala_Lumpur&mode=MONTH&src=dXRtYWlyb3N0LmNhaXJvQGdtYWlsLmNvbQ&color=%234285F4"  width="800" height="600" frameborder="0"></iframe>
+        </div>
     </div> );
 }
 
