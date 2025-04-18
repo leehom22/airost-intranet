4d80ec2a125e517eaaea1ed83adeef6f15f119c9
Ling Lee Hom <102334679+leehom22@users.noreply.github.com>
2025-04-18 09:30:45 +0800

    Edit task

--- client/src/pages/ProjectTracking/components/CardDetailsModal.js
+++ client/src/pages/ProjectTracking/components/CardDetailsModal.js
@@ -1,34 +1,30 @@
-
-
-
 import { BsPersonCircle } from "react-icons/bs";
 import { IoMdCreate } from "react-icons/io";
 import moment from "moment/moment";
 import consts from "../../../consts/consts";
 import { useState } from "react";
 import CardTitle from "./cardDetails/CardTitle";
+import CardDescription from "./cardDetails/CardDescription";
+import { useSelector } from "react-redux";
+import CardDueDate from "./cardDetails/CardDueDate";
+import CardPriority from "./cardDetails/CardPriority";
+import CardAsignee from "./cardDetails/CardAsignee";
 
 
 const CardDetailsModal = ({card}) => {
 
-    const [details,setDetail]=useState() //added
-    const [isEditing, setIsEdting] = useState(false); //added
-
     const priorityConsts = consts.priorityConsts;
+    
     return ( 
     <dialog id={`card-modal-${card._id}`} className="modal">
         <div className="modal-box bg-neutral-800 flex flex-col gap-1">
-            <CardTitle value={card.title}/> 
-            <p className="py-4">{card.description}</p>   {/*Description*/}
+            <CardTitle value={card.title} id={card._id}/> 
+            <CardDescription value={card.description} id={card._id}/>
             <div className="flex flex-row gap-1">
-                <p className={"text-xs w-min text-neutral-100 rounded-lg p-1 bg-neutral-900 text-neutral-400"}>
-                    {moment(card.dueDate).format("DD/MM/YYYY")} {/*Due Date*/}
-                </p>
-                <p className={`text-xs w-min text-neutral-100 rounded-lg p-1 ${priorityConsts[card.priority]?.bgColor}`}>
-                    {priorityConsts[card.priority]?.text} {/*Priority*/}
-                </p>
+                <CardDueDate value={card.dueDate} id={card._id}/>
+                <CardPriority value={card.priority} id={card._id}/>
             </div>
-            <p className="text-xs text-neutral-400 flex flex-row gap-1 items-center"><BsPersonCircle/>{card.assignee}</p> 
+            <CardAsignee value={card.assignee} id={card._id}/> 
             {/*Assignee*/}
             <p className="text-xs text-neutral-400 flex flex-row gap-1 items-center"><IoMdCreate/>{card.createdBy}</p>
             {/*Created By (No need to eidt)*/}
