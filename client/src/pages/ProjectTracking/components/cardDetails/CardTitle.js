c8ca53552910e9b7c211346d4265d39b18f3d30b
Ling Lee Hom <102334679+leehom22@users.noreply.github.com>
2025-04-18 09:30:10 +0800

    Edit task

--- client/src/pages/ProjectTracking/components/cardDetails/CardTitle.js
+++ client/src/pages/ProjectTracking/components/cardDetails/CardTitle.js
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setValue,setTitle } from "./ReduxStore";
 
 
-export default function CardTitle({value}){
-
+export default function CardTitle({value,id}){
     const [details,setDetail]=useState(value)
     const [isEditing, setIsEditing] = useState(false);
-    console.log(value)
-    const handleKeyDown=(e)=>{
+    const dispatch=useDispatch();
+    
+     const handleKeyDown=(e)=>{
         if(e.key==="Enter"){
+            dispatch(setTitle({id,title:details})) //Write to redux store 
             setIsEditing(false);
         }
         console.log(details)
     }
 
     const handleBlur=()=>{
+        dispatch(setTitle({id,title:details})) //Write to redux store 
         setIsEditing(false);
     }
 
@@ -24,12 +28,19 @@ export default function CardTitle({value}){
             (
                 <input
                 type="text"
+                id={id}
                 value={details}
                 autoFocus
-                onChange={(e)=>setDetail(e.target.value)}
-                style={{color: "black"}}
+                onChange={(e)=>{
+                    setDetail(e.target.value)
+                    dispatch(setTitle({id,title:e.target.value})) //Write to redux store 
+
+                }} //dispatch(setValue(e.target))
+                style={{color: "white"}}
                 onKeyDown={handleKeyDown}
                 onBlur={handleBlur}
+                className="font-bold text-lg bg-neutral-800"
+                
                 />
             )
             : <h3 className="font-bold text-lg" onClick={()=>setIsEditing(true)}>{details}</h3> 
