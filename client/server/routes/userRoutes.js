c415ea18bd12e582b48a1118f654bc842098a492
Ling Lee Hom <102334679+leehom22@users.noreply.github.com>
2025-05-10 23:00:03 +0800

    arrange file

--- client/server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { updateUser } = require('../controller/userController');
-const router = express.Router();
-
-//UPDATE a User
-router.patch('/:id', updateUser);
-
-module.exports = router;
